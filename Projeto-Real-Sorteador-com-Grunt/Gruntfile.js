b614f1f044e2adaa3604fbbb0a56ffd3505ef246
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2023-09-03 14:04:40 +0200

    update

--- Projeto-Real-Sorteador-com-Grunt/Gruntfile.js
+++ Projeto-Real-Sorteador-com-Grunt/Gruntfile.js
@@ -1,116 +1,105 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
-            development:{
-                files:{
-                    'dev/style/main.css': 'src/style/main.less'
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
-            production:{
-                options:{
-                    compress:true,
+            production: {
+                options: {
+                    compress: true,
                 },
-                files:{
-                    'dist/style/main.min.css':'src/style/main.less'
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
-            },
-        },
-        watch:{
-            less:{
-                files:['src/style/**/*.less'],
-                tasks:['less:development']
-            },
-            html:{
-                files:['src/index.html'],
-                tasks:['replace:dev']
             }
         },
-        replace:{
-            dev:{
-                options:{
-                    patterns:[
-                        {
-                        match: 'ENDERECO_DO_CSS',
-                        replacement: './style/main.css'
-                    },
-                    {
-                        match: 'ENDERECO_DO_JS',
-                        replacement: '../src/script/main.js'
-                    }
-                ]
-                },
-                files:[
-                    {
-                    expand:true,
-                    flatten:true,
-                    src:['src/index.html'],
-                    dest: 'dev/'
-                }
-            ]
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
             },
-            dist:{
-                options:{
-                    patterns:[
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
                         {
-                        match: 'ENDERECO_DO_CSS',
-                        replacement: './style/main.min.css'
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: './script/main.min.js'
+                            replacement: '../src/scripts/main.js'
                         }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
                 ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
                 },
-                files:[{
-                    expand:true,
-                    flatten:true,
-                    src:['prebuild/index.html'],
-                    dest: 'dist/'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
             }
-        }, 
-        htmlmin:{
-            dist:{
-                options:{
-                    removeComments:true,
-                    collapseWhitespace:true
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
                 },
-                files:{
-                    'prebuild/index.html':'src/index.html'
+                files: {
+                    'prebuild/index.html': 'src/index.html'
                 }
             }
         },
-        clean:['prebuild'],
-        uglify:{
-            target:{
-                files:{
-                    'dist/script/main.min.js':'src/script/main.js'
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         }
     })
 
-    //pluguin para executar o less
     grunt.loadNpmTasks('grunt-contrib-less');
-    
-    //pluguin para executar o watch dos arquivos
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-    //pluguin para podr ter 2 css dev e dist
     grunt.loadNpmTasks('grunt-replace');
-
-    //pluguin para fazer a miminificacao
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
-
-    //pluguin para apagar as pasta temporaria
     grunt.loadNpmTasks('grunt-contrib-clean');
-
-    //pluguin para comprimir os arquivos dist
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    //tarefas async executa quantas teras que eu quizer
-    grunt.registerTask('default',['watch']);
-    grunt.registerTask('build',['less:production','htmlmin:dist','replace:dist','clean','uglify']);
-
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
